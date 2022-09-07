192647404a3f99dcd04cf6ab2759942cf9a20b71
Ezzon <ezzon@ukr.net>
2022-09-07 11:29:57 +0300

    quest-1

---
+++ JavaApp/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+    double a = keyboard.nextDouble();
+    double b = keyboard.nextDouble();
+    double s = Math.pow(a, 2) + b * b;
+     System.out.println("Hypotenuse is " + s);
+    }
+}
